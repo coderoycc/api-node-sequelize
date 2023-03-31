716ed8601a1641fb36382e33bdda345f02f2f2e1
Roberto Chambi <rcchambi4@umsa.bo>
2023-03-30 22:08:33 -0400

    Variables de entorno - Conexión prueba

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import app from './app.js';
+import { config } from 'dotenv';
+config();
+import { sequelize } from './database/database.js';
 
-app.listen(3000, () => {
-  console.log("server is running on port 3000");
+
+app.listen(process.env.PORT, async () => {
+  console.log(`server is running on port ${process.env.PORT}`);
+  try {// prueba de la conexión de la base de datos
+    await sequelize.authenticate();
+    console.log('database is connected');
+  } catch (error) { 
+    console.log(error);
+  }
 })
